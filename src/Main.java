9fcb94be67f6d22b07233c43229a673700597b8b
EstebanGarcesA <esteg254@gmail.com>
2025-04-23 19:49:50 -0500

    Se complementa el ejercicio agregando los condicionales a cada programa y materia

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     //Debe permitir seleccionar la materia
     //Menu de opciones:
         //Desarrollo: Logica, metodologias, introduccion
-        //Diseño: Tipografia, iconografiam, semantica
+        //Diseño: Tipografia, iconografia, semantica
         //Gastronomia: Costos, panaderia dulce, cocina internacional
     //El calculo de notas es de la siguiente manera:
     //Para cada materia hay un examen parcial que equivale al 20% de la nota, un taller que equivale el 30% de la nota y un examen final que equivale el 50% de la nota
@@ -19,7 +19,8 @@ public class Main {
         //Si el estudiante hace un plan de mejoramiento, la nota maxima de la materia sera 3
    //El sistema debe discriminar la informacion general del estudiante
         //Nombre, documento y nota de la materia
-
+        double notaExamenParcial = 0, notaTaller = 0, notaExamenFinal = 0, notaFinal = 0;
+        String materia = "";
         System.out.println("Ingrese el nombre del estudiante");
         String nombreEstudiante = sc.nextLine();
         System.out.println("Ingrese el documento del estudiante");
@@ -29,24 +30,165 @@ public class Main {
         switch (programa){
             case "1":
                 System.out.println("Programa desarrollo de software \n \nEscriba el numero correspondiente a la materia \n1.Logica de programacion \n2.Metodologias agiles \n3.Introduccion a la programacion \n-----------");
-                String materia = sc.nextLine();
+                materia = sc.nextLine();
                 switch (materia){
                     case "1":
-                        System.out.println("Nota de logica de programacion");
+                        System.out.println("Notas de logica de programacion");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal= (notaExamenParcial * 0.2)+(notaTaller * 0.3)+(notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
                         break;
                     case "2":
-                        System.out.println("Nota de metodologias agiles");
+                        System.out.println("Notas de metodologias agiles");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal = (notaExamenParcial * 0.2)+(notaTaller * 0.3)+(notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
                         break;
                     case "3":
-                        System.out.println("Nota de Introduccion a la programacion");
+                        System.out.println("Notas de Introduccion a la programacion");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal = (notaExamenParcial * 0.2)+(notaTaller * 0.3)+(notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
                         break;
                 }
                 break;
             case "2":
-                System.out.println("Programa Diseño Grafico");
+                System.out.println("Programa Diseño Grafico \n \nEscriba el numero correspondiente a la materia \n1.Tipografia \n2.Iconografia \n3.Semantica \n-----------");
+                materia = sc.nextLine();
+                switch (materia) {
+                    case "1":
+                        System.out.println("Notas de Tipografia");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal = (notaExamenParcial * 0.2) + (notaTaller * 0.3) + (notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
+                        break;
+                    case "2":
+                        System.out.println("Notas de Iconografia");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal = (notaExamenParcial * 0.2) + (notaTaller * 0.3) + (notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
+                        break;
+                    case "3":
+                        System.out.println("Notas de Semantica");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal = (notaExamenParcial * 0.2) + (notaTaller * 0.3) + (notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
+                        break;
+                }
                 break;
             case "3":
-                System.out.println("Programa Gastronomia");
+                System.out.println("Programa Gastronomia \n \nEscriba el numero correspondiente a la materia \n1.Costos \n2.Panaderia Dulce \n3.Cocina Internacional \n-----------");
+                materia = sc.nextLine();
+                switch (materia) {
+                    case "1":
+                        System.out.println("Notas de Costos");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal = (notaExamenParcial * 0.2) + (notaTaller * 0.3) + (notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
+                        break;
+                    case "2":
+                        System.out.println("Notas de Panaderia Dulce");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal = (notaExamenParcial * 0.2) + (notaTaller * 0.3) + (notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
+                        break;
+                    case "3":
+                        System.out.println("Notas de Cocina Internacional");
+                        System.out.println("Nota exámen parcial: ");
+                        notaExamenParcial = sc.nextDouble();
+                        System.out.println("Nota Taller: ");
+                        notaTaller = sc.nextDouble();
+                        System.out.println("Nota Exámen final: ");
+                        notaExamenFinal = sc.nextDouble();
+                        notaFinal = (notaExamenParcial * 0.2) + (notaTaller * 0.3) + (notaExamenFinal * 0.5);
+                        if (notaFinal<=2.0){
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nperdio la materia");
+                        } else if (notaFinal<3.0) {
+                            System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nPerdio la materia, pero tiene derecho a plan de mejoramiento. Su nota maxima sera de 3.0");
+                        } else {System.out.println("El estudiante "+ nombreEstudiante+" Tiene la siguiente nota en la materia de logica de programación: " + notaFinal + "\nFelicidades, Ganaste!");
+                        }
+                        break;
+                }
                 break;
         }
 
